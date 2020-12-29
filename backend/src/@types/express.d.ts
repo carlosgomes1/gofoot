9865c314d0e67e468bd92d248b18d3e9a597d785
carlosgomes1 <carlos_gomes.s@hotmail.com>
2020-12-29 04:31:32 -0300

    Migration de fields ok

--- backend/src/@types/express.d.ts
+++ backend/src/@types/express.d.ts
@@ -1,5 +1,5 @@
 declare namespace Express {
   export interface Request {
-    idOwner: string
+    idOwner: string;
   }
 }
---
+++ backend/src/database/migrations/1609225348858-createFieldTable.ts
@@ -0,0 +1,76 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from "typeorm";
+
+export default class createFieldTable1609225348858
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "fields",
+        columns: [
+          {
+            name: "idField",
+            type: "varchar",
+            isPrimary: true,
+            generationStrategy: "uuid",
+          },
+          {
+            name: "name",
+            type: "varchar",
+            isNullable: false,
+          },
+          {
+            name: "logradouro",
+            type: "varchar",
+            isNullable: false,
+          },
+          {
+            name: "number",
+            type: "varchar",
+            isNullable: false,
+          },
+          {
+            name: "complement",
+            type: "varchar",
+            isNullable: true,
+          },
+          {
+            name: "latitude",
+            type: "int",
+            isNullable: false,
+          },
+          {
+            name: "longitude",
+            type: "int",
+            isNullable: false,
+          },
+          {
+            name: "fkOwner",
+            type: "uuid",
+            isNullable: false,
+          },
+        ],
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      "fields",
+      new TableForeignKey({
+        name: "foreignKeyOwner",
+        columnNames: ["fkOwner"],
+        referencedColumnNames: ["idOwner"],
+        referencedTableName: "owners",
+        onUpdate: "CASCADE",
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey("fields", "foreignKeyOwner");
+    await queryRunner.dropTable("fields");
+  }
+}
