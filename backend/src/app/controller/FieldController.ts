9c9d39a54d9993ddc3665466ea7e84376ef0e037
carlosgomes1 <carlos_gomes.s@hotmail.com>
2020-12-29 20:28:35 -0300

    Validação do cadastro de field com yup

--- backend/src/app/controller/FieldController.ts
+++ backend/src/app/controller/FieldController.ts
@@ -1,34 +1,51 @@
 import { Request, Response } from "express";
 import { getRepository } from "typeorm";
+import * as Yup from "yup";
 
 import Field from "../models/Field";
 
 class FieldController {
   async create(request: Request, response: Response) {
-    const repository = getRepository(Field);
-
-    const {
-      name,
-      logradouro,
-      number,
-      complement = "",
-      latitude,
-      longitude,
-    } = request.body;
-
-    const field = repository.create({
-      name,
-      logradouro,
-      number,
-      complement,
-      latitude,
-      longitude,
-      fkOwner: request.idOwner,
+    const schema = Yup.object().shape({
+      name: Yup.string().required("Name is required."),
+      logradouro: Yup.string().required("Logradouro is required."),
+      number: Yup.string().required("Number is required."),
+      complement: Yup.string(),
+      latitude: Yup.number().required("Latitude is required."),
+      longitude: Yup.number().required("Longitude is required."),
     });
 
-    await repository.save(field);
+    await schema
+      .validate(request.body)
+      .then(async () => {
+        const repository = getRepository(Field);
 
-    return response.json({ message: "Field created successfully.", field });
+        const {
+          name,
+          logradouro,
+          number,
+          complement = "",
+          latitude,
+          longitude,
+        } = request.body;
+
+        const field = repository.create({
+          name,
+          logradouro,
+          number,
+          complement,
+          latitude,
+          longitude,
+          fkOwner: request.idOwner,
+        });
+
+        await repository.save(field);
+
+        return response.json({ message: "Field created successfully.", field });
+      })
+      .catch(error => {
+        return response.status(400).json({ error: error.message });
+      });
   }
 }
 
