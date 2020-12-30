5d36ffa36e2edd313074feeea6d1d37f4834ead9
carlosgomes1 <carlos_gomes.s@hotmail.com>
2020-12-30 18:13:37 -0300

    Estilização de cadastro ok

---
+++ web/src/components/Input/styles.ts
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  border: 1px solid #034c26;
+
+  margin: 2px 0;
+
+  display: flex;
+  align-items: center;
+
+  padding: 10px;
+  border-radius: 8px;
+
+  width: 100%;
+
+  svg {
+    color: #034c26;
+    margin-right: 8px;
+  }
+
+  input {
+    border: 0;
+    background-color: transparent;
+
+    font-weight: 500;
+    font-size: 1.8rem;
+
+    color: #034c26;
+
+    &::placeholder {
+      color: #034c26;
+    }
+  }
+`;
--- web/src/pages/Register/index.tsx
+++ web/src/pages/Register/index.tsx
@@ -1,9 +1,35 @@
 import React from "react";
+import { FiMail, FiLock } from "react-icons/fi";
+import { FaWhatsapp, FaBuilding, FaMapMarkerAlt } from "react-icons/fa";
 
-// import { Container } from './styles';
+import { Container, Content } from "./styles";
+
+import JogadorSVG from "../../assets/images/jogador-de-futebol.svg";
+
+import Header from "../../components/Header";
+import Input from "../../components/Input";
 
 const Register: React.FC = () => {
-  return <h1>Register</h1>;
+  return (
+    <Container>
+      <Header />
+      <Content>
+        <img src={JogadorSVG} alt="Jogador de futebol" />
+        <div>
+          <h1>Registre-se aqui!</h1>
+          <form>
+            <Input placeholder="E-mail" name="email" icon={FiMail} />
+            <Input placeholder="Senha" name="password" icon={FiLock} />
+            <Input placeholder="WhatsApp" name="whatsapp" icon={FaWhatsapp} />
+            <Input placeholder="Estado" name="uf" icon={FaMapMarkerAlt} />
+            <Input placeholder="Cidade" name="city" icon={FaBuilding} />
+
+            <button type="submit">Cadastrar</button>
+          </form>
+        </div>
+      </Content>
+    </Container>
+  );
 };
 
 export default Register;
