7ef41ae148aedfb25625b37405b52b869def9b91
carlosgomes1 <carlos_gomes.s@hotmail.com>
2020-12-30 20:40:06 -0300

    Habilitando cors na api

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,7 @@ import "./database";
 import * as dotenv from "dotenv";
 
 import express from "express";
+import cors from "cors";
 
 import routes from "./routes";
 
@@ -11,6 +12,7 @@ const port = process.env.SERVER_PORT;
 
 dotenv.config();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
