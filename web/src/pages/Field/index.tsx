b348bb0e1ce4f74f7f6e2c72b2c418fa5989efae
carlosgomes1 <carlos_gomes.s@hotmail.com>
2020-12-31 20:04:46 -0300

    Update de field ok

--- web/src/pages/Field/index.tsx
+++ web/src/pages/Field/index.tsx
@@ -1,12 +1,20 @@
-import React, { useEffect, useCallback, useState } from "react";
+import React, { useEffect, useCallback, useState, useRef } from "react";
+import { Map, Marker, TileLayer } from "react-leaflet";
+import { LeafletMouseEvent } from "leaflet";
+import { FormHandles } from "@unform/core";
 import { useLocation } from "react-router-dom";
+import * as Yup from "yup";
 
-import { Container, Content } from "./styles";
+import { Container, Content, Update, InputContainer, Left } from "./styles";
 
 import api from "../../services/api";
+import getValidationErrors from "../../utils/getValidationErrors";
+
+import { useAuth } from "../../hooks/auth";
 
 import Header from "../../components/Header";
 import Aside from "../../components/Aside";
+import Input from "../../components/Input";
 
 interface LocationProps {
   idField: string;
@@ -30,16 +38,80 @@ interface ResponseDataField {
   responsibles: Responsible[];
 }
 
+interface DataFormProps {
+  name: string;
+  logradouro: string;
+  number: string;
+  complement?: string;
+  latitude: string;
+  longitude: string;
+}
+
 const Field: React.FC = () => {
+  const [selectedPosition, setSelectedPosition] = useState<[number, number]>([
+    0,
+    0,
+  ]);
   const [field, setField] = useState<ResponseDataField>(
     {} as ResponseDataField,
   );
 
   const { idField } = useLocation().state as LocationProps;
 
+  const formRef = useRef<FormHandles>(null);
+  const { token } = useAuth();
+
+  const sendUpdate = useCallback(
+    async (data: DataFormProps) => {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+
+      await api.put(`/field/${field.idField}`, data, config);
+    },
+    [token, field.idField],
+  );
+
+  const handleSubmit = useCallback(
+    async (data: DataFormProps) => {
+      try {
+        formRef.current?.setErrors({});
+
+        const schema = Yup.object().shape({
+          name: Yup.string().required("O nome da quadra é obrigatório"),
+          logradouro: Yup.string().required("O logradouro é obrigatório."),
+          number: Yup.string().required("O número é obrigatório."),
+          complement: Yup.string(),
+          latitude: Yup.number().required("A latitude é obrigatório."),
+          longitude: Yup.number().required("A longitude é obrigatório."),
+        });
+
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        sendUpdate(data);
+      } catch (err) {
+        const errors = getValidationErrors(err);
+
+        formRef.current?.setErrors(errors);
+      }
+    },
+    [sendUpdate],
+  );
+
+  function handleMapClick(event: LeafletMouseEvent): void {
+    setSelectedPosition([event.latlng.lat, event.latlng.lng]);
+    formRef.current?.setFieldValue("latitude", event.latlng.lat);
+    formRef.current?.setFieldValue("longitude", event.latlng.lng);
+  }
+
   const getField = useCallback(async () => {
     await api.get<ResponseDataField>(`/field/${idField}`).then((response) => {
       setField(response.data);
+      setSelectedPosition([response.data.latitude, response.data.longitude]);
     });
   }, [idField]);
 
@@ -52,7 +124,68 @@ const Field: React.FC = () => {
       <Header />
       <Content>
         <Aside selected="field" />
-        <h1>{field.name}</h1>
+        <Update>
+          <Left
+            ref={formRef}
+            initialData={{
+              name: field.name,
+              logradouro: field.logradouro,
+              number: field.number,
+              complement: field.complement,
+              latitude: field.latitude,
+              longitude: field.longitude,
+            }}
+            onSubmit={handleSubmit}
+          >
+            <InputContainer>
+              <span>Nome</span>
+              <Input name="name" />
+            </InputContainer>
+            <InputContainer>
+              <span>Logradouro</span>
+              <Input name="logradouro" />
+            </InputContainer>
+            <InputContainer>
+              <span>Número</span>
+              <Input name="number" />
+            </InputContainer>
+            <InputContainer>
+              <span>Complemento</span>
+              <Input name="complement" />
+            </InputContainer>
+            <InputContainer>
+              <span>Latitude</span>
+              <Input name="latitude" />
+            </InputContainer>
+            <InputContainer>
+              <span>Longitude</span>
+              <Input name="longitude" />
+            </InputContainer>
+            <div>
+              <button type="submit">Alterar</button>
+            </div>
+          </Left>
+          <Map
+            center={[field.latitude || 0, field.longitude || 0]}
+            zoom={13}
+            style={{
+              width: "100%",
+              height: 500,
+              margin: "8px 0",
+              borderRadius: 8,
+              boxShadow:
+                "0px 4px 6px -1px rgba(0,0,0,0.1) , 0px 2px 4px -1px rgba(0,0,0,0.06)",
+            }}
+            onClick={handleMapClick}
+            scrollWheelZoom
+          >
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={selectedPosition} />
+          </Map>
+        </Update>
       </Content>
     </Container>
   );
--- web/src/pages/Field/styles.ts
+++ web/src/pages/Field/styles.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { Form } from "@unform/web";
 
 import WaveSVG from "../../assets/images/wave.svg";
 
@@ -19,8 +20,48 @@ export const Content = styled.div`
   flex: 1;
 
   width: 80%;
+`;
 
-  h1 {
-    flex: 1;
+export const Update = styled.div`
+  flex: 1;
+
+  display: flex;
+  align-items: center;
+`;
+
+export const Left = styled(Form)`
+  display: flex;
+  flex-direction: column;
+
+  padding: 0 16px;
+
+  div button {
+    background-color: #034c26;
+    color: #eaeaea;
+
+    padding: 10px;
+    margin-top: 8px;
+
+    width: 100%;
+    font-size: 1.8rem;
+
+    border-radius: 8px;
+    border: 0;
+
+    transition: background-color 0.3s;
+
+    &:hover {
+      background-color: #01381b;
+    }
+  }
+`;
+
+export const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  span {
+    font-size: 1.6rem;
+    font-weight: 500;
   }
 `;
