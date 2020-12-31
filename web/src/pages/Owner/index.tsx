6fbd94407b0c91a60c3a594c8a214738de2e1368
carlosgomes1 <carlos_gomes.s@hotmail.com>
2020-12-31 01:38:16 -0300

    Listando fields do owner ok

--- web/src/pages/Owner/index.tsx
+++ web/src/pages/Owner/index.tsx
@@ -1,16 +1,80 @@
-import React from "react";
+import React, { useEffect, useState, useCallback } from "react";
+import { FiTrash2, FiSettings } from "react-icons/fi";
+
+import api from "../../services/api";
+import { useAuth } from "../../hooks/auth";
 
 import Header from "../../components/Header";
 import Options from "../../components/Options";
 
-import { Container, Content } from "./styles";
+import { Container, Content, Fields, FieldItem } from "./styles";
+
+interface Field {
+  complement?: string;
+  fkOwner: string;
+  idField: string;
+  latitude: string;
+  longitude: string;
+  name: string;
+  number: string;
+  logradouro: string;
+}
+
+interface ResponseFields {
+  fields: Field[];
+  total: number;
+}
 
 const Owner: React.FC = () => {
+  const [fields, setFields] = useState<Field[]>([]);
+  const [totalFields, setTotalFields] = useState(0);
+
+  const { token } = useAuth();
+
+  const getFields = useCallback(async () => {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    await api.get<ResponseFields>("/fieldOfOwner", config).then((response) => {
+      setFields(response.data.fields);
+      setTotalFields(response.data.total);
+    });
+  }, [token]);
+
+  useEffect(() => {
+    getFields();
+  }, [getFields]);
+
   return (
     <Container>
       <Header />
       <Content>
         <Options selected="owner" />
+        <h1>Você possui {totalFields} campos.</h1>
+
+        <Fields>
+          {fields.map((field) => (
+            <FieldItem key={field.idField}>
+              <div>
+                <FiTrash2 size={32} />
+                <FiSettings size={32} />
+              </div>
+              <h1>Nome</h1>
+              <span>{field.name}</span>
+              <h1>Logradouro</h1>
+              <span>{field.logradouro}</span>
+              <h1>Número</h1>
+              <span>{field.number}</span>
+              <h1>Complemento</h1>
+              <span>
+                {field.complement ? field.complement : "Sem complemento"}
+              </span>
+            </FieldItem>
+          ))}
+        </Fields>
       </Content>
     </Container>
   );
--- web/src/pages/Owner/styles.ts
+++ web/src/pages/Owner/styles.ts
@@ -15,7 +15,68 @@ export const Container = styled.div`
 
 export const Content = styled.div`
   width: 80%;
+  height: auto;
+  flex: 1;
 
   display: flex;
+  flex-direction: column;
   align-items: center;
+
+  h1 {
+    font-size: 3.2rem;
+    margin: 24px 0;
+  }
+`;
+
+export const Fields = styled.ul`
+  flex: 1;
+  list-style: none;
+  width: 100%;
+
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  place-items: center;
+
+  padding-bottom: 24px;
+
+  grid-gap: 24px;
+`;
+
+export const FieldItem = styled.li`
+  box-shadow: 6px 2px 16px 0px rgba(136, 165, 191, 0.48),
+    -6px -2px 16px 0px rgba(255, 255, 255, 0.8);
+
+  width: 100%;
+
+  padding: 8px;
+  min-height: 280px;
+
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    svg {
+      cursor: pointer;
+      color: #034c26;
+    }
+  }
+
+  div svg:first-of-type {
+    color: #c53030;
+    margin-right: 4px;
+  }
+
+  h1 {
+    font-size: 1.6rem;
+    color: #034c26;
+    margin: 0;
+
+    margin-top: 8px;
+  }
+
+  span {
+    font-size: 2rem;
+    color: #666666;
+  }
 `;
