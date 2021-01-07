20284546e9124f90a04d033bb998e8655e5aafff
carlosgomes1 <carlos_gomes.s@hotmail.com>
2021-01-07 13:07:24 -0300

    Novo projeto mobile arrumando bugs, eslint e prettier configurados

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,6 +1,15 @@
-import React from "react";
 import { StatusBar } from "expo-status-bar";
+import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+
+const App: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -10,13 +19,5 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
 });
-const App: React.FC = () => {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-};
 
 export default App;
